2a0187f42dc8e3de45042c9e774b0fd68173e8c9
Leif Stenfeldt <leif@stenfeldt.dk>
2018-06-04 17:40:52 +0200

    Tilpassede det regulære udtryk, så den aflæser værdien med 00.0000 cifre.

--- aktiekurs.php
+++ aktiekurs.php
@@ -1,9 +1,11 @@
 <?php
 	$homepage = @file_get_contents("http://www.msn.com/da-dk/finans/aktieoplysninger/fi-172.1.VELO.CSE");
 
-	print "<pre>";
-	print_r($homepage);
-	preg_match('/<span.*\"currentvalue\">(\d{1,3},\d{1,2})<\/span>/i', $homepage, $matches); 
+	preg_match('/<span.*\"currentvalue\">(\d{1,3},\d{1,4})<\/span>/i', $homepage, $matches); 
+
+//Search for valuee in:	<span class="currentval" data-role="currentvalue">0,8980</span>
+
+
 	$resultat =  ($matches!=null) ? floatval(str_replace(",", ".",$matches[1])):"Error";
-	echo $resultat;
+	echo "VELO.CSE : " . $resultat;
 ?>
